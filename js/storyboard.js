808f91b9b44b2aae8121e2683c741b5e9e30bd28
AmishAssassin <dennis@pantsdownsoftware.com>
2015-10-06 15:20:09 -0400

    Add in some sample files

---
+++ js/storyboard.js
@@ -0,0 +1,10 @@
+﻿var domain = "http://dinova-uat.azurewebsites.net/";
+
+formatPhone = function (s) {
+    if (s != null && s != "") {
+        return "(" + s.substr(0, 3) + ") " + s.substr(3, 3) + " - " + s.substr(6, 4);
+    }
+}
+
+
+$(".change-password").click(function () { $(document).trigger("CHANGE_PASSWORD"); });
